1d20655301d4e33a9a79250a5d4b2b7b76c5b21d
anamritraj <anandamritraj904@gmail.com>
2017-01-29 18:49:19 +0530

    Script working now. Will need a few fixes.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+from subprocess import call
 
 # Global Variables
 
 URL = 'http://www.geeksforgeeks.org/tag/amazon/page/'
 # TODO: Make the number of pages dynamic
-PAGES = 30
+PAGES = 1
 
-to_crawl = []
-crawled = []
+links_to_crawl = []
+file_names = []
 
 
 def get_crawl_links():
     page = 1
-    while page < PAGES:
+    while page <= PAGES:
         url = URL + str(page)
         print("Getting from Page Number: " + str(page))
         # Create a Beautiful Soup Object
@@ -21,7 +22,8 @@ def get_crawl_links():
 
         links = soup.select(".entry-title > a")
         for link in links:
-            to_crawl.append(link.get('href'))
+            links_to_crawl.append(link.get('href'))
+            file_names.append(link.text.replace("|", "-"))
         page += 1
 
 
@@ -30,8 +32,22 @@ def create_soup(url):
     return BeautifulSoup(r.text, 'html.parser')
 
 
+def generate_html():
+    i = 0
+    for link in links_to_crawl:
+        print("Getting file" + file_names[i])
+        # Uses wkhtmltopdf which is installed via a windows excecutable. Files are saved in D drive.
+        PATH_TO_WKHTML = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe "
+        OUTPUT_FOLDER = "D:\\Study\\Amazon1\\"
+        command = PATH_TO_WKHTML + "\"" + link + "\" \"" + OUTPUT_FOLDER + file_names[i] + ".pdf\""
+
+        call(command)
+        i += 1
+
+
 def main():
     get_crawl_links()
+    generate_html()
 
 if __name__ == '__main__':
     main()
