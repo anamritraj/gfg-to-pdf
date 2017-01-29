beb4ccdd0b91571e05844358d910b69c69750380
anamritraj <anandamritraj904@gmail.com>
2017-01-29 15:51:51 +0530

    Get links list

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Global Variables
+
+URL = 'http://www.geeksforgeeks.org/tag/amazon/page/'
+# TODO: Make the number of pages dynamic
+PAGES = 30
+
+to_crawl = []
+crawled = []
+
+
+def get_crawl_links():
+    page = 1
+    while page < PAGES:
+        url = URL + str(page)
+        print("Getting from Page Number: " + str(page))
+        # Create a Beautiful Soup Object
+        soup = create_soup(url)
+
+        links = soup.select(".entry-title > a")
+        for link in links:
+            to_crawl.append(link.get('href'))
+        page += 1
+
 
 def create_soup(url):
     r = requests.get(url)
@@ -8,9 +31,7 @@ def create_soup(url):
 
 
 def main():
-    url = 'http://www.geeksforgeeks.org/tag/amazon/page/1/'
-    soup = create_soup(url)
-    print(soup.title.text)
+    get_crawl_links()
 
 if __name__ == '__main__':
     main()
